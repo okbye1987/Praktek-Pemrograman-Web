61ffa56a11991ddcc0fe3e47543b10f7eaa176af
okbye1987 <120629829+okbye1987@users.noreply.github.com>
2022-12-15 15:12:59 +0700

    Add files via upload
    push

---
+++ OKBYE TRIBUDIAWAN - 2170234022 - PAP/Pertemuan_4/tugas/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Form</title>
+</head>
+<body>
+    <form method="POST" action="tugas3.php">
+    <table width="400" align="center" celpadding="2" cellspacing="2">
+    <tr><td  width="130">Login Name :</td><td><input type="text" name="nama"></td></tr>
+    <tr><td>
+        <input type="submit" name="btnLogin" value="Login">
+    </table>
+    </form>
+</body>
+</html>
